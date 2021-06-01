a4e9c7271c28be9984c9bf627b61f07fc297ad99
PavelGritsenko <pasha.zp.ua94@gmail.com>
2021-06-01 21:52:55 +0300

    HW_10

---
+++ index.js
@@ -0,0 +1,128 @@
+var quotient = 66 / 33;
+console.log(quotient)
+
+var myVar = 87;
+myVar++;
+console.log(myVar)
+
+var myVar = 11;
+myVar--;
+console.log(myVar)
+
+var myDecimal = 5.7;
+console.log(myDecimal)
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var quotient = 4.4 / 2.0;
+console.log(quotient)
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
+
+var a = 3;
+var b = 17;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+console.log(a)
+console.log(b)
+console.log(c)
+
+var a = 11;
+var b = 9;
+var c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+console.log(a)
+console.log(b)
+console.log(c)
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+console.log(a)
+console.log(b)
+console.log(c)
+
+var a = 48;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a)
+console.log(b)
+console.log(c)
+
+var myFirstName = 'Pavel';
+var myLastName  = 'Gritsenko';
+console.log(myFirstName)
+console.log(myLastName)
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr)
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr)
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+console.log(myStr)
+
+/*
+var myStr = 'This is the start. ' + 'This is the end.';
+console.log(myStr)
+
+var myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.';
+console.log(myStr)
+
+var myName = 'Pavel';
+var myStr = 'My name is ' + myName + ' and I am well!';
+console.log(myStr)
+
+var someAdjective = 'ffgfcgfc';
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(myStr)
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+lastNameLength = lastName.length;
+console.log(lastNameLength)
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName)
+
+var myStr = "Jello World";
+myStr = "Hello World";
+console.log(myStr)
+
+var lastName = "Lovelace";
+var thirdLetterOfLastName = lastName[2]; 
+console.log(thirdLetterOfLastName)
+
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1];
+console.log(lastLetterOfLastName)
+
+var lastName = "Lovelace";
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+console.log(secondToLastLetterOfLastName)
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+var wordBlanks = 'my ' + myNoun + ' is ' + myAdjective + ' and ' + myVerb + ' very ' + myAdverb;
+console.log(wordBlanks)
+*/
