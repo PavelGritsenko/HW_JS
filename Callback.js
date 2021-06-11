2293ed11af7832de00a396f159693f588ff6cecd
PavelGritsenko <pasha.zp.ua94@gmail.com>
2021-06-11 10:20:17 +0300

    Separate functions

--- Callback.js
+++ Callback.js
@@ -2,10 +2,16 @@ function calc (func, param1, param2) {
     return func(param1, param2)
 }
 
-console.log(calc ((param$1, param$2) => param$1 + param$2, 6, 3 ))
-console.log(calc ((param$1, param$2) => param$1 - param$2, 6, 3 ))
-console.log(calc ((param$1, param$2) => param$1 * param$2, 6, 3 ))
-console.log(calc ((param$1, param$2) => param$1 / param$2, 6, 3 ))
-console.log(calc ((param$1, param$2) => param$1 ** param$2, 6, 3 ))
-console.log(calc ((param$1, param$2) => param$1 % param$2, 6, 3 ))
+let sum = (param$1, param$2) => param$1 + param$2;
+let diff = (param$1, param$2) => param$1 - param$2;
+let mult = (param$1, param$2) => param$1 * param$2;
+let div = (param$1, param$2) => param$1 / param$2;
+let expo = (param$1, param$2) => param$1 ** param$2;
+let rem = (param$1, param$2) => param$1 % param$2;
 
+console.log(calc (sum, 6, 3 ));
+console.log(calc (diff, 6, 3 ));
+console.log(calc (mult, 6, 3 ));
+console.log(calc (div, 6, 3 ));
+console.log(calc (expo, 6, 3 ));
+console.log(calc (rem, 6, 3 ));
